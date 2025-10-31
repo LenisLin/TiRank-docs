4439e1804b21e5db3c6a3e7966063e6164d2ee41
Lin-Yux <727682308@qq.com>
2025-10-31 16:09:32 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,8 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = '_static/TiRank_white.png'
 
+nbsphinx_execute = 'never'
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
