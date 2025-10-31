4690f77eebb7a5247316db3589f8ef7331cfa980
Lin-Yux <727682308@qq.com>
2025-10-31 15:58:33 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,67 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TiRank'
+copyright = '2025, LenisLin'
+author = 'LenisLin'
+release = '1.0'
 
-release = '0.1'
-version = '0.1.0'
-
-# -- General configuration
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'myst_parser',
+    'sphinx.ext.autodoc',       # Core: pulls documentation from docstrings
+    'sphinx.ext.autosummary',   # Core: creates summary tables
+    'sphinx.ext.napoleon',      # Enables Sphinx to read Google-style docstrings
+    'sphinx.ext.viewcode',      # Adds a "[source]" link next to your functions
+    'nbsphinx'
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "myst",
 }
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+exclude_patterns = []
 
-# -- Options for HTML output
+
+# Tell autosummary to auto-generate stub files
+autosummary_generate = True
+
+# Good defaults so module pages have member anchors & clean ordering
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "private-members": False,
+    "show-inheritance": True,
+    "inherited-members": True,
+    "member-order": "bysource",
+}
+
+# Google-style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = '_static/TiRank_white.png'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+autodoc_mock_imports = [
+    "torch", "torchvision", "torchaudio",
+    "cupy", "cudnn", "pytorch_lightning",
+    "timm", "scanpy", "anndata", "igraph", "leidenalg",
+]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,22 +1,79 @@
-Welcome to Lumache's documentation!
-===================================
+.. TiRank documentation master file
 
-**Lumache** (/lu'make/) is a Python library for cooks and food lovers
-that creates recipes mixing random ingredients.
-It pulls data from the `Open Food Facts database <https://world.openfoodfacts.org/>`_
-and offers a *simple* and *intuitive* API.
+==================================================
+TiRank prioritizes phenotypic niches in tumor microenvironment for clinical biomarker discovery
+==================================================
 
-Check out the :doc:`usage` section for further information, including
-how to :ref:`installation` the project.
+.. image:: _static/TiRank_white.png
+    :width: 50%
+    :align: center
+    :alt: TiRank Logo
 
-.. note::
+|
 
-   This project is under active development.
+.. image:: https://img.shields.io/pypi/v/tirank?style=flat-square
+    :target: https://pypi.org/project/TiRank/
+    :alt: PyPI
+.. image:: https://img.shields.io/github/license/LenisLin/TiRank?style=flat-square
+    :target: https://github.com/LenisLin/TiRank/blob/main/LICENSE
+    :alt: License
+.. image:: https://readthedocs.org/projects/tirank/badge/?version=latest&style=flat-square
+    :target: https://tirank.readthedocs.io/en/latest/
+    :alt: Documentation Status
 
-Contents
---------
+TiRank is a cutting-edge toolkit designed to integrate and analyze RNA-seq and single-cell RNA-seq (scRNA-seq) data. 
+By seamlessly combining spatial transcriptomics or scRNA-seq data with bulk RNA sequencing data, 
+TiRank enables researchers to identify phenotype-associated regions or clusters. 
+The toolkit supports various analysis modes, including survival analysis (Cox), classification, and regression, providing a comprehensive solution for transcriptomic data analysis.
+
+.. image:: _static/FigS1.png
+    :width: 100%
+    :align: center
+    :alt: TiRank Workflow
+
+----
 
 .. toctree::
+    :maxdepth: 2
+    :caption: Getting Started
 
-   usage
-   api
+    installation
+    model_input
+
+.. toctree::
+    :maxdepth: 2
+    :caption: CLI Tutorials
+
+    tutorial_st_survival
+    tutorial_sc_classification
+
+.. toctree::
+    :maxdepth: 2
+    :caption: GUI Tutorial
+
+    tutorial_web
+
+.. toctree::
+    :maxdepth: 1
+    :caption: Features Document
+
+    result_interpretation
+    hyperparameters
+
+.. toctree::
+    :maxdepth: 1
+    :caption: API Reference
+
+    api
+    
+----
+
+Citing & Support
+================
+
+For support, please visit the `TiRank GitHub Issues page <https://github.com/LenisLin/TiRank/issues>`.
+
+License
+=======
+
+TiRank is distributed under the MIT License.
